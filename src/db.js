2a7a225116be2307d5009c5493309254921f6967
kepita1818 <kepaizaguirre991@gmail.com>
2026-06-12 16:58:03 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,12 +1,15 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const { Pool } = pg;
 
+const isRemoteDb = process.env.DATABASE_URL?.includes('sslmode=require');
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes('sslmode=require') ? { rejectUnauthorized: false } : false
+  ssl: isRemoteDb ? { rejectUnauthorized: false } : false
 });
 
 export async function query(text, params = []) {
