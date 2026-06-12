9244fd05f05ceb9004aaeb4f329a31b671c78f23
kepita1818 <kepaizaguirre991@gmail.com>
2026-06-12 17:23:08 +0200

    Update import-daily.js

--- scripts/import-daily.js
+++ scripts/import-daily.js
@@ -4,15 +4,33 @@ import { recalculateUpcomingFeatures } from '../src/services/featureService.js';
 
 dotenv.config();
 
-const DAILY_COMPETITIONS = ['ENG1', 'ENG2', 'ESP1', 'ESP2', 'ITA1', 'ITA2', 'GER1', 'GER2', 'FRA1', 'FRA2', 'POR1', 'NED1'];
+const DAILY_COMPETITIONS = [
+  'WC2026',
+  'INTL',
+  'ENG1', 'ENG2',
+  'ESP1', 'ESP2',
+  'ITA1', 'ITA2',
+  'GER1', 'GER2',
+  'FRA1', 'FRA2',
+  'POR1', 'NED1'
+];
 
 async function run() {
-  await importFootballData({ competitionsFilter: DAILY_COMPETITIONS, years: 2 });
+  await importFootballData({
+    competitionsFilter: DAILY_COMPETITIONS,
+    years: 2,
+    includeFutureFixtures: true,
+    includeScheduled: true
+  });
+
   await recalculateUpcomingFeatures(14);
+
   console.log('Daily import completed');
 }
 
-run().then(() => process.exit(0)).catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+run()
+  .then(() => process.exit(0))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
